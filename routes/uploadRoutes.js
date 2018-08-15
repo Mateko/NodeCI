e3b5b1c7446ad4fc359ee28bf0362aaef55fa38b
Mateko <mateuszpierzchala01@gmail.com>
2018-08-15 16:17:53 +0200

    AWS S3

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,20 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1')
+const keys = require('../conf/keys')
+const requireLogin = require('../middlewares/requireLogin')
+const s3 = new AWS.S3({
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey
+})
+module.exports = app => {
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl(
+      'putObject', {
+      Bucket: 'x-x',
+      ContentType: 'image/jpeg',
+      Key: key
+    }, (err, url) => res.send({key, url}));
+  });
+};
